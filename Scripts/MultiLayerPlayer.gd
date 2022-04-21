716e25895974ee55dbcd8aa6cfc9d4b4d42f5e1f
Geegaz <geegazrd@gmail.com>
2022-04-21 22:31:17 +0200

    various changes

--- Scripts/MultiLayerPlayer.gd
+++ Scripts/MultiLayerPlayer.gd
@@ -3,26 +3,26 @@ class_name MultiLayerPlayer
 
 export(Array, AudioStream) var streams
 
-onready var _Players: Array = create_players()
+onready var _Layers: Array = create_layers()
 onready var _Tween = Tween.new()
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	add_child(_Tween)
-	for player in _Players:
-		add_child(player)
+	for layer in _Layers:
+		add_child(layer)
 
 func set_layer_active(layer:int, active:bool, time:float = 1.0)->void:
-	if layer >= _Players.size():
+	if layer >= _Layers.size():
 		return
-	var target_player: AudioStreamPlayer = _Players[layer]
-	_Tween.remove(target_player, "volume_db")
+	var target_layer: AudioStreamPlayer = _Layers[layer]
+	_Tween.remove(target_layer, "volume_db")
 	
 	if time > 0:
 		_Tween.interpolate_property(
-			target_player, 
+			target_layer, 
 			"volume_db",
-			target_player.volume_db, 
+			target_layer.volume_db,
 			volume_db if active else -82,
 			time,
 			Tween.TRANS_CUBIC,
@@ -31,38 +31,39 @@ func set_layer_active(layer:int, active:bool, time:float = 1.0)->void:
 		if not _Tween.is_active():
 			_Tween.start()
 	else:
-		target_player.volume_db = volume_db if active else -82
+		target_layer.volume_db = volume_db if active else -82
 
 #func set_layer_volume(layer:int, volume:float)->void:
-#	if layer >= _Players.size():
+#	if layer >= _Layers.size():
 #		return
-#	var target_player: AudioStreamPlayer = _Players[layer]
+#	var target_player: AudioStreamPlayer = _Layers[layer]
 #	target_player.volume_db = volume
 
+
 func play(from_position:float = 0.0)->void:
-	for player in _Players:
-		player.play(from_position)
+	for layer in _Layers:
+		layer.play(from_position)
 
 func seek(position: float)->void:
-	for player in _Players:
-		player.seek(position)
+	for layer in _Layers:
+		layer.seek(position)
 
 func stop()->void:
-	for player in _Players:
-		player.stop()
+	for layer in _Layers:
+		layer.stop()
 
 
-func create_players() -> Array:
-	if _Players and _Players.size() > 0:
-		for player in _Players:
-			player.free()
+func create_layers() -> Array:
+	if _Layers and _Layers.size() > 0:
+		for layer in _Layers:
+			layer.free()
 	
-	var new_players := []
+	var new_layers := []
 	for layer in streams:
 		# Duplicate the node only by using instancing
-		var new_player: AudioStreamPlayer = duplicate(8)
-		new_player.stream = layer
-		new_player.volume_db = -82
-		new_players.append(new_player)
+		var new_layer: AudioStreamPlayer = duplicate(8)
+		new_layer.stream = layer
+		new_layer.volume_db = -82
+		new_layers.append(new_layer)
 	
-	return new_players
+	return new_layers
