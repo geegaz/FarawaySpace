10a1a1fbd9f16fa7201703b9c7c0f5e74c2c7f39
Geegaz <geegazrd@gmail.com>
2022-05-02 14:01:18 +0200

    fixes on AudioArea and Manager

--- Scripts/AudioArea.gd
+++ Scripts/AudioArea.gd
@@ -2,6 +2,9 @@ extends Area
 
 export(String) var target_group: String = "Player"
 export(NodePath) var associated_player: NodePath
+export(float, 0, 60) var margin_time: float = 30
+
+var exited_time: float = 0.0
 
 onready var _AssociatedPlayer: AudioStreamPlayer = get_node_or_null(associated_player)
 
@@ -9,10 +12,19 @@ func _ready() -> void:
 	connect("body_entered",self,"_on_InteriorArea_body_entered")
 	connect("body_exited",self,"_on_InteriorArea_body_exited")
 
+func _process(delta: float) -> void:
+	exited_time = max(exited_time - delta, 0)
+
 func _on_InteriorArea_body_entered(body: Node) -> void:
 	if body.is_in_group(target_group):
-		AudioManager.fade_to(_AssociatedPlayer)
+		AudioManager.fade_to(_AssociatedPlayer, 6.0)
+		
+		if exited_time <= 0:
+			_AssociatedPlayer.play()
 
 func _on_InteriorArea_body_exited(body: Node) -> void:
 	if body.is_in_group(target_group):
-		AudioManager.fade_to(AudioManager._DefaultPlayer)
+		if _AssociatedPlayer == AudioManager._CurrentPlayer:
+			AudioManager.fade_to(AudioManager._DefaultPlayer, 3.0)
+		
+		exited_time = margin_time
--- Scripts/AudioManager.gd
+++ Scripts/AudioManager.gd
@@ -7,32 +7,30 @@ onready var _CurrentPlayer: AudioStreamPlayer = _DefaultPlayer
 onready var _Tween: Tween = $Tween
 
 func fade_to(new_player: AudioStreamPlayer, time = 1.0):
-	if new_player == _CurrentPlayer:
-		return
-	
 	if time > 0:
 		if new_player:
 			# Setup tween for audio fading in
 			_Tween.remove(new_player)
 			_Tween.interpolate_property(
 				new_player, "volume_db",
-				new_player.volume_db, linear2db(1),
-				time
+				new_player.volume_db, 0,
+				time, Tween.TRANS_EXPO, Tween.EASE_OUT
 			)
 		if _CurrentPlayer:
 			# Setup tween for audio fading out
 			_Tween.remove(_CurrentPlayer)
 			_Tween.interpolate_property(
 				_CurrentPlayer, "volume_db",
-				_CurrentPlayer.volume_db, linear2db(0),
-				time
+				_CurrentPlayer.volume_db, -80,
+				time, Tween.TRANS_EXPO, Tween.EASE_IN
 			)
 		# Start the tween
 		if not _Tween.is_active():
 			_Tween.start()
 	else:
-		_CurrentPlayer.volume_db = linear2db(0)
-		new_player.volume_db = linear2db(1)
+		_CurrentPlayer.volume_db = -80
+		new_player.volume_db = 0
 	
 	# Change the current player
 	_CurrentPlayer = new_player
+	print("fading to",_CurrentPlayer,_CurrentPlayer.name)
