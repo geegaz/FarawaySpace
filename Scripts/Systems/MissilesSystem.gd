64c472c0f392263a87092e0c86cfd77637b17626
Geegaz <geegazrd@gmail.com>
2022-11-18 18:27:00 +0100

    Started reworking the MissilesSystem

--- Scripts/Systems/MissilesSystem.gd
+++ Scripts/Systems/MissilesSystem.gd
@@ -1,8 +1,6 @@
 extends Spatial
 
-export var targeting_system: NodePath
-export var missile: PackedScene
-export var hit_effect: PackedScene
+export var hit_method: String = "missile_hit"
 # Missile parameters
 export var random_start_velocity: float = 0.05
 export var use_parent_velocity: = true
@@ -11,14 +9,28 @@ export var shoot_cooldown: = 0.25
 # Effect parameters
 export(float, 0.0, 1.0) var screenshake_directional: float = 0.8
 export(float, 1.0, 100.0) var screenshake_distance_multiplier: float = 50.0
+export var missile: PackedScene
+export var hit_effect: PackedScene
 
+class Missile:
+	var lifetime: float
+	var damping: float
+	var thrust_force: float
+	
+	var transform: Transform
+	var velocity: Vector3
+	var target: Spatial
+	var life: float = lifetime
+	
 
 var cooldown: = 0.0
 var target_index: int = 0
 
 onready var _Missiles: Node = $Missiles
 onready var _Effects: Node = $Effects
-onready var _TargetingSystem: Spatial = get_node(targeting_system) as Spatial
+
+#export var targeting_system: NodePath
+#onready var _TargetingSystem: Spatial = get_node(targeting_system) as Spatial
 
 func _ready():
 	pass
@@ -26,25 +38,30 @@ func _ready():
 func _process(delta):
 	cooldown = max(cooldown - delta, 0.0)
 
-func _unhandled_input(event):
-	if event is InputEventMouseButton and event.button_index == BUTTON_WHEEL_DOWN:
-		if event.pressed and cooldown == 0.0:
-			fire_missile()
-			cooldown = shoot_cooldown
+# Shooting should be handled by the parent - or any other object
+#func _unhandled_input(event):
+#	if event is InputEventMouseButton and event.button_index == BUTTON_WHEEL_DOWN:
+#		if event.pressed and cooldown == 0.0:
+#			fire_missile()
+#			cooldown = shoot_cooldown
 
-func fire_missile():
+func fire_missile(target: Spatial = null):
 	# Don't fire if there is no missile set
-	if not missile:
+	# or if the cooldown is not finished yet
+	if not missile or cooldown > 0.0:
 		return
+	cooldown = shoot_cooldown
 	
 	var new_missile: Spatial = missile.instance()
 	new_missile.transform = global_transform
 	
-	if _TargetingSystem:
-		var targets: Array = _TargetingSystem.get_visible_targets()
-		if targets.size() > 0:
-			target_index = (target_index + 1) % targets.size()
-			new_missile.target = targets[target_index]
+	# Target should be provided by the parent - or any other object
+#	if _TargetingSystem:
+#		var targets: Array = _TargetingSystem.get_visible_targets()
+#		if targets.size() > 0:
+#			target_index = (target_index + 1) % targets.size()
+#			new_missile.target = targets[target_index]
+	new_missile.target = target
 	
 	# Set random velocity
 	var random_angle: float = randf() * TAU
