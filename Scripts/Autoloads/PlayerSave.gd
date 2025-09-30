92f618ca555fa75eb2e4db2d7e6b6c0f4964c071
geegaz <geegazrd@gmail.com>
2025-09-30 16:44:54 +0200

    Added PlayerSave autoload

---
+++ Scripts/Autoloads/PlayerSave.gd
@@ -0,0 +1,67 @@
+extends Node
+
+enum {
+	SAVE_TYPE_BINARY,
+	SAVE_TYPE_JSON
+}
+
+const SAVE_PATHS: = {
+	SAVE_TYPE_BINARY: "user://save.dat",
+	SAVE_TYPE_JSON: "user://save.json"
+}
+
+var save_data: = {}
+var save_type: = SAVE_TYPE_JSON
+var save_path: String : 
+	get : return SAVE_PATHS[save_type]
+
+var save_requested: = false
+
+func _init() -> void:
+	# Load the save file as soon as PlayerSave is initialized, so classes 
+	# using saved data can use it the moment they're loaded.
+	# Create the save file if it doesn't exist yet.
+	if FileAccess.file_exists(save_path):
+		load_file()
+	else:
+		save_file()
+
+func get_saved(value_name: StringName, default: Variant = null)->Variant:
+	return save_data.get(value_name, default)
+
+func set_saved(value_name: StringName, value: Variant) -> bool:
+	var success: = save_data.set(value_name, value)
+	if success:
+		request_save()
+	return success
+
+
+func save_file()->void:
+	var file: = FileAccess.open(save_path, FileAccess.WRITE)
+	match save_type:
+		SAVE_TYPE_BINARY:
+			file.store_var(save_data)
+		SAVE_TYPE_JSON:
+			var json_string: = JSON.stringify(save_data, "\t")
+			file.store_string(json_string)
+	print("Saved save file with data:\n",save_data)
+
+func load_file()->void:
+	var file: = FileAccess.open(save_path, FileAccess.READ)
+	match save_type:
+		SAVE_TYPE_BINARY:
+			save_data = file.get_var()
+		SAVE_TYPE_JSON:
+			var json_string: = file.get_as_text()
+			save_data = JSON.parse_string(json_string)
+	print("Loaded save file with data:\n",save_data)
+
+func request_save()->void:
+	if save_requested:
+		return
+	# Wait for the end of the current frame before saving the file.
+	# This should prevent multiple file writes in the same frame.
+	save_requested = true
+	await get_tree().process_frame
+	save_file()
+	save_requested = false
--- Scripts/LevelElements/LevelDoor.gd
+++ Scripts/LevelElements/LevelDoor.gd
@@ -2,10 +2,15 @@ class_name LevelDoor
 extends Node3D
 
 @export var level: LevelData
-@export var locks: Array # (Array, String)
-@export var petal_animations: Array[AnimationPlayer]
-@export var petal_animation_name: String = "DoorPetal_ARMAction"
+@export var required_keys: PackedStringArray
 @export var door_animation: AnimationPlayer
+
+var available_keys: PackedStringArray:
+	set(value): PlayerSave.set_saved("available_keys", value)
+	get: return PlayerSave.get_saved("available_keys", [])
+var used_keys: PackedStringArray:
+	set(value): PlayerSave.set_saved("used_keys", value)
+	get: return PlayerSave.get_saved("used_keys", [])
 
 var open: bool
 
@@ -17,6 +22,18 @@ func _enter_tree() -> void:
 	door_trigger.body_entered.connect(_on_door_trigger_body_entered)
 	door_trigger.body_exited.connect(_on_door_trigger_body_exited)
 
+func _ready() -> void:
+	var unlocked: = true
+	for key in required_keys:
+		if key not in used_keys:
+			unlocked = false
+			break
+	
+	if unlocked:
+		open = true
+		door_animation.play("open_door")
+		door_animation.seek(3.0) # Go to the end of the animation
+
 func _on_door_trigger_body_entered(body: Node)->void:
 	if body is Ship:
 		open_door()
@@ -27,6 +44,52 @@ func _on_door_trigger_body_exited(body: Node)->void:
 
 
 func open_door()->void:
-	if not open:
-		door_animation.play("open_door")
-		open = true
+	if open:
+		return
+	
+	# Get a reference to the key lists to avoid
+	# calling the getter on each iteration
+	var a_keys: = available_keys
+	var u_keys: = used_keys
+	
+	if a_keys.is_empty():
+		# TODO: Feedback for the missing keys
+		return # No key available, so no need to check them
+	
+	var required_keys_amount: = required_keys.size()
+	for key in required_keys:
+		if key in u_keys:
+			required_keys_amount -= 1
+		elif key in a_keys:
+			a_keys.erase(key)
+			u_keys.append(key)
+			required_keys_amount -= 1
+			# TODO: Feedback for the used keys that were removed
+	
+	# Apply the changed keys to the save file
+	available_keys = a_keys
+	used_keys = u_keys
+	
+	if required_keys_amount > 0:
+		# TODO: Feedback for the missing keys
+		return # Not enough keys
+	
+	open = true
+	door_animation.play("open_door")
+
+
+func debug_force_open()->void:
+	open = true
+	door_animation.play("open_door")
+
+func debug_add_keys()->void:
+	var a_keys: = available_keys
+	var u_keys: = used_keys
+	
+	for key in required_keys:
+		if key in a_keys or key in u_keys:
+			continue
+		a_keys.append(key)
+	
+	available_keys = a_keys
+	
